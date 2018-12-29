5d01aa0dec0523934986a243302b0334a668e603
Jimmy Spark <jimmy8developer@gmail.com>
2018-12-29 14:35:27 +0330

    Design PersonalInfoActivity xml file

--- NiNiGraph/app/src/main/java/ir/ninigraph/ninigraph/Activity/PersonalInfoActivity.java
+++ NiNiGraph/app/src/main/java/ir/ninigraph/ninigraph/Activity/PersonalInfoActivity.java
@@ -1,9 +1,11 @@
 package ir.ninigraph.ninigraph.Activity;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import ir.ninigraph.ninigraph.R;
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class PersonalInfoActivity extends AppCompatActivity {
 
@@ -11,5 +13,10 @@ public class PersonalInfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal_info);
+    }
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
     }
 }
